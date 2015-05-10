778e56a3d4951d9001d9c10bcf6634502bd05225
gozu66 <gozu66@gmail.com>
2015-05-11 00:01:38 +0100

    Character Controller, Basic player & camera movement & logic

---
+++ Assets/_Scripts/Player/Movement/Prototyping/BasicCamFollow1.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicCamFollow1 : MonoBehaviour 
+{
+	public Transform lookPoint, movePoint;					//POINT TO LOOK TOWARDS		//POINT TO MOVE TOWARDS
+	public float camMoveSpeed = 0.3f, Sensitivity = 1.0f;	//CAMERA MOVESPEED			//CAMERA LOOK SENSITIVITY
+
+	Transform myTransform;	//CAMERA TRANSFORM
+	Rigidbody camRbody;		//CAMERA RIGIDBODY
+
+	void Start()
+	{
+		camRbody = GetComponent<Rigidbody>();				//CACHE CAMERA TRANSFORM
+		myTransform = GetComponent<Transform>();			//CACHE CAMERA RIGIDBODY
+	}
+
+	void Update()
+	{
+		myTransform.LookAt(lookPoint.position, (Vector3.up));										//LOOK TOWARDS TARGET
+		camRbody.MovePosition(Vector3.Lerp(transform.position, movePoint.position, camMoveSpeed));	//MOVE TOWARDS TARGET
+
+		if(Input.GetAxis("Mouse ScrollWheel") != 0)
+		{
+			movePoint.localPosition += new Vector3(0f, 0f, Input.GetAxis("Mouse ScrollWheel"));
+		}
+	}
+}
---
+++ Assets/_Scripts/Player/Movement/Prototyping/BasicMove1.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicMove1 : MonoBehaviour 
+{
+	public float speed, strafeSpeed, rotationSpeed;		//MOVEMENT SPEED	//ROTATION SPEED
+
+	Rigidbody myRbody;			//RIGIDBODY COMPONENT
+	Transform myTransform;		//TRANSFORM COMPONENT
+
+	void Start () 
+	{
+		myRbody = GetComponent<Rigidbody>();			//CACHING RIGIDBODY COMPONENT
+		myTransform = GetComponent<Transform>();		//CACHING TRANSFORM COMPONENT
+	}
+	
+	void FixedUpdate () 
+	{
+		myRbody.AddForce(myTransform.forward 	* 	(speed			*	Input.GetAxis("Vertical")));		//FORWAR+BACK PHYSICS FORCES INPUT
+		myRbody.AddForce(myTransform.right		*	(strafeSpeed	*	Input.GetAxis("Horizontal")));		//RIGHT + LEFT PHYSICS FORCES INPUT
+
+		myRbody.AddTorque(myTransform.up		*	(rotationSpeed	*	Input.GetAxis("Mouse X")));			//ROTATIONAL PHYSICS FORCES INPUT
+		myRbody.AddTorque(myTransform.up		*	(rotationSpeed	*	Input.GetAxis("Horizontal")));			//ROTATIONAL PHYSICS FORCES INPUT
+	}
+}
