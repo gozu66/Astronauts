2d1de958c44e5e11de555856334142a26171f9a8
gozu66 <gozu66@gmail.com>
2015-05-15 22:23:57 +0100

    Player controller + camera upgrade, camera mouse orbiting, turning, jumping.

---
+++ Assets/_Scripts/Player/Movement/Prototyping/PlayerCamera.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCamera : MonoBehaviour 
+{
+	public Transform lookPoint, movePoint, cameraPivot;			//POINT TO LOOK TOWARDS		//POINT TO MOVE TOWARDS
+	public float camMoveSpeed = 0.3f, Sensitivity = 1.0f;		//CAMERA MOVESPEED			//CAMERA LOOK SENSITIVITY
+
+	Transform myTransform;	//CAMERA TRANSFORM
+	Rigidbody camRbody;		//CAMERA RIGIDBODY
+
+	void Start()
+	{
+		camRbody = GetComponent<Rigidbody>();					//CACHE CAMERA TRANSFORM
+		myTransform = GetComponent<Transform>();				//CACHE CAMERA RIGIDBODY
+		myTransform.position = movePoint.position;				//MOVE CAMERA TO PLAYER ON START
+	}
+
+	void Update()
+	{
+		myTransform.LookAt(lookPoint.position, (Vector3.up));																//LOOK TOWARDS TARGET
+
+		if(!PlayerMove.playerIsMoving && camRbody.velocity.magnitude <= 1)													//IF PLAYER+CAM HAVE STOPPED MOVING
+		{
+			myTransform.RotateAround(cameraPivot.position, Vector3.up, (Sensitivity*Input.GetAxis("Mouse X")));				//ENABLE MOUSE ORBIT
+		}else {
+			camRbody.MovePosition(Vector3.Lerp(transform.position, movePoint.position, (camMoveSpeed*Time.deltaTime)));		//ELSE LERP CAMERA TO MOVEPOINT
+		}
+
+	}
+}
---
+++ Assets/_Scripts/Player/Movement/Prototyping/PlayerMove.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMove : MonoBehaviour 
+{
+	public float speed, strafeSpeed, rotationSpeed;				//MOVEMENT SPEED	//ROTATION SPEED
+	public float mouseSensitivity = 1.0f, jumpForce;
+	float V, H;
+
+	Rigidbody myRbody;											
+	Transform myTransform, cameraTransf;						//CAMERA TRANSFORM  //TRANSFORM COMPONENT
+
+	public static bool playerIsMoving;
+
+	void Start () 
+	{
+		myRbody = GetComponent<Rigidbody>();					//CACHING RIGIDBODY
+		myTransform = GetComponent<Transform>();				//CACHING TRANSFORM
+		cameraTransf = Camera.main.GetComponent<Transform>();	//CACHING CAMERA TRANSFORM
+	}
+	
+	void FixedUpdate () 
+	{			
+		myRbody.AddForce(myTransform.forward * (speed*Input.GetAxis("Vertical")));					//FORWAR+BACK PHYSICS FORCES INPUT
+
+		transform.Rotate(0, Input.GetAxis("Horizontal")*rotationSpeed, 0);							//ROTATIONAL FORCES ON KEYS
+
+		if(V != 0 && H == 0)
+			transform.Rotate(0, (Input.GetAxisRaw("Mouse X")*rotationSpeed)*mouseSensitivity, 0);	//ROTATIONAL FORCES ON MOUSE (WHEN MOVING)
+
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			myRbody.velocity += new Vector3(0, jumpForce, 0);
+			Debug.Log("SPACE");
+		}
+	}
+
+	void Update()
+	{
+		V = Input.GetAxis("Vertical");
+		H = Input.GetAxis("Horizontal");
+
+		if(V != 0 || H != 0){
+			playerIsMoving = true;
+		}
+		if(V == 0 && H == 0){
+			playerIsMoving = false;		
+		}
+	}
+}
