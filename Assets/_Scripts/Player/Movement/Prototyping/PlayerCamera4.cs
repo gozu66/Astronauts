e98e8d6c1ad5f66e711f244676031972955a4208
gozu66 <gozu66@gmail.com>
2015-05-23 17:53:53 +0100

    Sideline...Isometric Camera View and Movement first pass

---
+++ Assets/_Scripts/Player/Movement/Prototyping/PlayerCamera4.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCamera4 : MonoBehaviour 
+{
+	public float speed;
+
+	Transform playerTarget, myTransform;
+	Camera _camera;
+
+	int xMin = 200, yMin = 50;
+	int xMax, yMax;
+	Vector3 refV3 = Vector3.zero, Disatance;
+	bool X, Y;
+	float DistX, DistY;
+
+	void Start()
+	{
+		myTransform = GetComponent<Transform>();
+		_camera = GetComponent<Camera>();
+		playerTarget = FindObjectOfType<PlayerMove4>().transform;
+		xMax = _camera.pixelWidth - xMin;
+		yMax = _camera.pixelHeight - (yMin * 2);
+	}
+
+	void Update()
+	{
+		Vector3 playerScreenPos = _camera.WorldToScreenPoint(playerTarget.position);
+
+		DistX = Mathf.Abs(playerTarget.position.x - myTransform.position.x);
+		DistY = Mathf.Abs(playerTarget.position.y - myTransform.position.y);
+		Debug.Log(DistX + "X  " + DistY + "Y");
+
+		if(playerScreenPos.x > xMax || playerScreenPos.x < xMin)
+		{
+			if(!X)
+				StartCoroutine("ScrollX");
+		}
+			
+		if(playerScreenPos.y > yMax || playerScreenPos.y < yMin)
+		{
+			if(!Y)
+				StartCoroutine("ScrollY");
+		}
+	}
+
+	IEnumerator ScrollX()
+	{
+		X = true;
+		yield return null;
+		X = false;
+	}
+
+	IEnumerator ScrollY()
+	{
+		Y = true;
+		yield return null;
+		Y = false;
+	}
+
+}
---
+++ Assets/_Scripts/Player/Movement/Prototyping/PlayerMove4.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMove4 : MonoBehaviour 
+{
+	public float speed, strafeSpeed, rotationSpeed;										//MOVEMENT SPEED	//ROTATION SPEED
+	public float mouseSensitivity = 1.0f, jumpForce;
+	float V, H;
+	
+	Rigidbody myRbody;											
+	Transform myTransform;																//TRANSFORM COMPONENT
+	
+	public Transform camPivot, playerMesh;
+	
+	public static bool playerIsMoving;
+	public static bool isGrounded;
+	
+	void Start () 
+	{
+		myRbody = GetComponent<Rigidbody>();											//CACHING RIGIDBODY
+		myTransform = GetComponent<Transform>();										//CACHING TRANSFORM
+	}
+	
+	void FixedUpdate () 																//FIXED UPDATE FOR PHYSICS
+	{	
+		myRbody.AddForce(Vector3.forward * (speed*Input.GetAxis("Vertical")));			//FWD + BCK PHYSICS FORCES + INPUT
+		myRbody.AddForce(Vector3.right * (speed*Input.GetAxis("Horizontal")));			//FWD + BCK PHYSICS FORCES + INPUT
+		
+		if(playerIsMoving)
+		{
+			Quaternion newFace = Quaternion.LookRotation(new Vector3(myRbody.velocity.x, 0, myRbody.velocity.z));
+			playerMesh.rotation = Quaternion.Slerp(playerMesh.rotation, newFace, rotationSpeed);//(new Vector3(myRbody.velocity.x, 0, myRbody.velocity.z) * Time.deltaTime);
+		}
+	}
+	
+	void Update()
+	{
+		V = Input.GetAxis("Vertical");													//CACHING AXES
+		H = Input.GetAxis("Horizontal");												//...
+		
+		if(Input.GetKeyDown(KeyCode.Space) && isGrounded)								//.. 
+		{
+			myRbody.AddForce(new Vector3(0, jumpForce, 0), ForceMode.Impulse);		
+		} 
+		
+		if(V != 0 || H != 0){
+			playerIsMoving = true;														//SETTING STAIC BOOL "PLAYERISMOVING"
+		}																				//ON INPUT
+		if(V == 0 && H == 0){															//..
+			playerIsMoving = false;														//..
+		}																				//..
+		
+		RaycastHit hit;
+		if(Physics.Raycast(transform.position, -Vector3.up, out hit, 1.5f))				//..
+		{																				//RAYCASH FOR GROUNDCHECK
+			isGrounded = true;															//GROUNDED BOOLEAN
+		} else {																		//..
+			isGrounded = false;															//..
+		}																				//..
+	}
+}
