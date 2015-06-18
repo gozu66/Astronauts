2abcef20620c72101a783c28b1d13588247dda5e
gozu66 <gozu66@gmail.com>
2015-06-18 23:13:44 +0100

    Update for Meeting

---
+++ Assets/_Scripts/Enviroments/SpinOverTime.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpinOverTime : MonoBehaviour 
+{
+	public float spinSpeed;
+	public bool doSpin;
+	Transform myT;
+
+	void Start()
+	{
+		myT = this.GetComponent<Transform>();
+	}
+
+
+	void Update()
+	{
+	if(doSpin)
+		myT.Rotate(myT.up, spinSpeed * Time.deltaTime);
+	}
+}
